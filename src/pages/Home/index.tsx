aa1eb3f78b41e7f5ec1e4dd853ab60a8d2a0a3db
Marcio Oliveira <zamar_pinheiro@hotmail.com>
2023-05-06 04:46:15 -0300

    up

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import CheckSvg from '../../../public/check_circle_svg.svg'
 export function Home() {
     return (
         <Container>
+            <h1 style={{ justifyContent: "center", textAlign: "center", color: "red" }}>Em Construção</h1>
             <Introtext>Evepay: o futuro do pagamento</Introtext>
             <IntroParagraph>Já aconteceu com você, ir na padaria pela manhã e na hora de pagar, cadê o cartão ? Celular ?
                 Sem problemas, com a Evepay você paga com sua digital e nós gerenciamos os meios de pagamento cadastrados por você.
